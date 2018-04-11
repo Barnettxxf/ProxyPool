0377dcfbef367e98603406b9a494510dda88543a
Barnettxxf <15102096586@163.com>
2018-04-11 18:48:49 +0800

    modify get_ip

--- ProxyPool/utils/get_ip.py
+++ ProxyPool/utils/get_ip.py
@@ -1,24 +1,9 @@
 # -*- coding:utf-8 -*-
 
-# from .common import MysqlConnection
-#
-#
-# class GetIp(MysqlConnection):
-#
-#     @property
-#     def ip_list(self):
-#         return self.get_ip()
-#
-#     def get_ip(self):
-#         query_sql = """
-#             select ip,port from proxyhttps order by date_time DESC limit 20;
-#         """
-#         self.execute(query_sql)
-#         return self.cursor.fetchall()
 from sqlalchemy import desc
 
-from ProxyPool.ProxyPool.model.available import FilterIP
-from ProxyPool.ProxyPool.model import loadSession
+from ProxyPool.model.available import FilterIP
+from ProxyPool.model import loadSession
 
 
 class AbtainIp(object):
