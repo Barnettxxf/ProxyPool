3502a41dad49f503f7b10b0908b36985969266b0
Barnettxxf <15102096586@163.com>
2018-04-12 00:36:36 +0800

    fixed bug

--- ProxyPool/middlewares.py
+++ ProxyPool/middlewares.py
@@ -19,9 +19,7 @@ class RotateUserAgentMiddleware(UserAgentMiddleware):
         ua = random.choice(self.user_agent_list)
         print('User-Agent: ', ua)
         request.headers.setdefault('User-Agent', ua)
-        # request.headers.setdefault('Refer', refer)
 
-    # for more user agent strings,you can find it in http://www.useragentstring.com/pages/useragentstring.php
     user_agent_list = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
         "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
@@ -45,30 +43,27 @@ class RotateUserAgentMiddleware(UserAgentMiddleware):
 
 
 class SeleniumMiddleware(object):
-    driver = webdriver.Chrome(chrome_options=option)
 
     def __init__(self, timeout=10, script_timeout=20):
         self.timeout = timeout
         self.script_timeout = script_timeout
+        self.driver = webdriver.Chrome(chrome_options=option)
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(timeout=crawler.settings.get('SELENIUM_TIMEOUT', 10),
-                   script_timeout=crawler.settings.get('selenium_script_timeout', 20))
+        s = cls(timeout=crawler.settings.get('SELENIUM_TIMEOUT', 10),
+               script_timeout=crawler.settings.get('selenium_script_timeout', 20))
+        crawler.signals.connect(s.spider_closed, signal=signals.spider_closed)
+        return s
 
     def spider_opened(self, spider):
         self.driver.set_page_load_timeout(self.timeout)
         self.driver.set_script_timeout(self.script_timeout)
 
     def process_request(self, request, spider):
-        # try:
         for rule in spider.rule:
             if rule.allow_domains in request.url and rule.selenium_enable:
                 return self.download(request)
-        # except TimeoutError:
-        #     return request
-        # except AttributeError:
-        #     return self.download()
 
     def spider_closed(self):
         self.driver.quit()
